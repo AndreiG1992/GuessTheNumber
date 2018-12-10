7a5590607d949b717790125bec094845835e9e9d
Andrei Gandila <andrei.gandila@yahoo.com>
2018-12-11 00:27:05 +0200

    Add message if the number is too low or too high, a counting score variable. Add gameover question

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -10,34 +10,60 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
-            int yourChoice, number;
             
-            Random random = new Random();
-            number = random.Next(1,100);
+
             Console.Write("\t\t\t\t*********************************************************************\n");
             Console.Write("\t\t\t\t*\t\t\t\t\t\t\t\t\t*\n");
             Console.Write("\t\t\t\t*\t\t\t\tGUESS THE NUMBER\t\t\t*\n");
             Console.Write("\t\t\t\t*\t\t\t\t\t\t\t\t\t*\n");
             Console.Write("\t\t\t\t*********************************************************************\n");
+
             Console.WriteLine("What's Your Name?");
             string name = Console.ReadLine();
             Console.WriteLine($"Hi {name}, let's play \"Guess the number\"");
-            do {
+            
+            restart:
+            int yourChoice, number, score = 1;
+            Random random = new Random();
+            number = random.Next(1, 100);
+            do
+            {
+                
                 Console.Write("Choose a number from 1 to 100: ");
                 yourChoice = int.Parse(Console.ReadLine());
-                if(yourChoice < 1 || yourChoice >100)
+                if (yourChoice < 1 || yourChoice > 100)
                 {
                     Console.WriteLine("Wrong number! Your choice must be from 1 to 100");
                     continue;
                 }
+                else
+                {
+                    if (yourChoice == number)
+                    {
+                        Console.WriteLine($"Right answer! {number} is the correct number");
+                        Console.WriteLine($"You've succeeded in {score} attempts");
+                        break;
+                    }
+                    else if (yourChoice < number)
+                    {
+                        Console.WriteLine("Too low! Try Again!");
+                        ++score;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too high! Try Again");
+                        ++score;
+                    }
 
-                if (yourChoice == number) {
-                    Console.WriteLine($"Congrats! You did it. {yourChoice} is the correct number");
-                    break;
                 }
-                else Console.WriteLine("Try Again!\n");
-
             } while (yourChoice != number);
+            Console.WriteLine("\n\nGame Over!\nWant another try?[y/n]");
+            char optionToContinue = Convert.ToChar(Console.ReadLine());
+            if (optionToContinue == 'y') goto restart;
+            if (optionToContinue == 'n') Console.WriteLine("Goodbye");
+
+
         }
+
     }
 }
